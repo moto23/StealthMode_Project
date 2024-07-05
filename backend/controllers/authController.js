5b6a3883b4060fa0e8f17e306fa12d29bf974851
Prasad Nathe <99172728+moto23@users.noreply.github.com>
2024-07-05 18:09:16 +0530

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -33,7 +33,7 @@ const requestPasswordReset = async (req, res) => {
     subject: 'Password Reset',
     text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n
            Please click on the following link, or paste this into your browser to complete the process within one hour of receiving it:\n\n
-           http://localhost:3001/reset-password/${token}\n\n
+           https://sleath-frontend.vercel.app/reset-password/${token}\n\n
            If you did not request this, please ignore this email and your password will remain unchanged.\n`
   };
 
